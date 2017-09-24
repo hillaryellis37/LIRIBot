5fcc911dddc19bd27e7d6f2af1c3963efb9f2c41
Live Sound Systems <hillaryellis37@gmail.com>
2017-09-23 18:28:44 -0700

    BOOM SHAKA - added Spotify API

--- keys.js
+++ keys.js
@@ -1,11 +1,14 @@
 console.log('this is loaded');
 
-var twitterKeys = {
-  consumer_key: 'xioXky74pojO7NuWbloUUgi1j',
-  consumer_secret: 'PbPlYXF1of3rnPUZlfvGmmIfwSiYJVuPdkd6TCxOmKJ19GB41h',
-  access_token_key: '911059886166781952-sOxp8LEsufmnSMWvN96sw62tdHUi9zO',
-  access_token_secret: 'riiEOPt8OtqVTBrMtPbdrR9uwUtz7pCR3CTihEoOcingn',
+var Keys = {
+  twitter_consumer_key: 'xioXky74pojO7NuWbloUUgi1j',
+  twitter_consumer_secret: 'PbPlYXF1of3rnPUZlfvGmmIfwSiYJVuPdkd6TCxOmKJ19GB41h',
+  twitter_access_token_key: '911059886166781952-sOxp8LEsufmnSMWvN96sw62tdHUi9zO',
+  twitter_access_token_secret: 'riiEOPt8OtqVTBrMtPbdrR9uwUtz7pCR3CTihEoOcingn',
+
+  spotify_client_ID: '51213b4252c447929683d52988f6b9c9',
+  spotify_client_secret: '1915b9073fbd4b90bb057f757aab24ae',
 }
 
-module.exports = twitterKeys;
+module.exports = Keys;
 
--- liri.js
+++ liri.js
@@ -1,38 +1,75 @@
 var keys = require("./keys.js");
 var arg1 = process.argv[2];
+var songName = process.argv[3];
 var keysArray = [];
 
 
-// my-tweets:
-
-// accessing key elements from keys.js:
+// accessing ALL key elements (Twitter & Spotify) from keys.js with for loop and throwing each key into the keysArray:
 
 for (var key in keys) {
   keysArray.push(keys[key]);
 }
 
-
-var Twitter = require('twitter');
+// Twitter section:
  
-var client = new Twitter({
-  consumer_key: keysArray[0],
-  consumer_secret: keysArray[1],
-  access_token_key: keysArray[2],
-  access_token_secret: keysArray[3]
-});
+if (arg1 === "my-tweets") {
+
+	var Twitter = require('twitter');
  
-var params = {screen_name: 'HillZillary'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
+	var client = new Twitter({
+	  consumer_key: keysArray[0],
+	  consumer_secret: keysArray[1],
+	  access_token_key: keysArray[2],
+	  access_token_secret: keysArray[3]
+	});
 
-  	for (var i = 0; i < tweets.length; i++) {
-  		console.log(tweets[i].text);
-  	}
-  
-  }
-});
+	var params = {screen_name: 'HillZillary'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
 
-// spotify-this-song
+	  	for (var i = 0; i < tweets.length; i++) {
+	  		console.log(tweets[i].text);
+	  	}
+	  
+	  }
+	});
+}
+
+else if (arg1 === "spotify-this-song") {
+
+	var Spotify = require('node-spotify-api');
+	 
+	var spotify = new Spotify({
+	  id: keysArray[4],
+	  secret: keysArray[5]
+	});
+	 
+	spotify.search({ type: 'track', query: songName }, function(err, data) {
+	  if (err) {
+	    return console.log('Error occurred: ' + err);
+	  }
+
+		var artistArray = data.tracks.items[0].artists;
+		var artists = '';
+	// creating a 4-loop here to capture all artists and concatenate them to a string in artists variable:
+		for (var i = 0; i < artistArray.length; i++) {
+			artists = artists + " " + data.tracks.items[0].artists[0].name;
+		}
+
+		var song = data.tracks.items[0].name;
+		var previewURL = data.tracks.items[0].preview_url;
+		var album = data.tracks.items[0].album.name;
+
+		console.log(
+			"Artist(S): ", artists,
+			"\nSong Title: ", song,
+			"\nPreview Link: ", previewURL,
+			"\nAlbum; ", album);
+	});
+}
+// spotify-this-song section
+
+
 
 // movie-this
 
